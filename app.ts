8d47e1e8aa7b9ac370ba213644096c27ac9abb6c
Yong Li <yong.li1992@foxmail.com>
2018-06-17 12:51:45 +0100

    the 'any' type

--- app.ts
+++ app.ts
@@ -39,3 +39,13 @@ enum Color {
 
 let myColor: Color = Color.Blue;
 console.log(myColor);
+
+// any, try to avoid any
+let car: any = "BMW";
+console.log(car);
+car = { // re-assign to another type
+    brand: "BMW",
+    series: 3
+};
+console.log(car);
+
