f5165fb9fe27de105fc831bc200c7e0f1cc44499
Yong Li <yong.li1992@foxmail.com>
2018-06-17 11:33:28 +0100

    assigning types explicitly

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ var myAge = 27.5;
 // myAge = 'Max'; // error
 // boolean
 var hasHobbies = false;
-hasHobbies = 1;
+// hasHobbies = 1; // still error, TS doesn't do implicit casting
+// assign types
+// let myRealAge; // if I don't assign a value initially, TS treats it as type Any
+// myRealAge = 26; // can assign a number
+// myRealAge = '27'; // can assign a string
+// we don't get any advantages of TS, if we simply use type Any.
+var myRealAge;
+myRealAge = 27;
+myRealAge = '27';
--- app.ts
+++ app.ts
@@ -1,12 +1,21 @@
 // string
-let myName = 'Yong';
+let myName: string = 'Yong';
 // myName = 28; // error, can't reassign a different type
 
 // number
-let myAge = 27.5;
+let myAge: number = 27.5;
 // myAge = 'Max'; // error
 
 // boolean
 let hasHobbies = false;
 // hasHobbies = 1; // still error, TS doesn't do implicit casting
 
+// assign types
+// let myRealAge; // if I don't assign a value initially, TS treats it as type Any
+// myRealAge = 26; // can assign a number
+// myRealAge = '27'; // can assign a string
+// we don't get any advantages of TS, if we simply use type Any.
+
+let myRealAge: number; // explicitly assign type
+myRealAge = 27;
+// myRealAge = '27';
