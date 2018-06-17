0b9360f8676a0025dda0131d18d3eb08bd516b64
Yong Li <yong.li1992@foxmail.com>
2018-06-17 17:49:09 +0100

    class methods and access modifiers

--- exercise.js
+++ exercise.js
@@ -1,14 +1,16 @@
-var bankAccount = {
-    money: 2000,
-    deposit: function (value) {
-        this.money += value;
-    }
-};
-var myself = {
-    name: "Max",
-    bankAccount: bankAccount,
-    hobbies: ["Sports", "Cooking"]
-};
-myself.bankAccount.deposit(3000);
-console.log(myself);
+// type AccountType = { money: number, deposit: (val: number) => void };
+// let bankAccount: AccountType = {
+//     money: 2000,
+//     deposit(value) {
+//         this.money += value;
+//     }
+// };
+// type Person = { name: string, bankAccount: AccountType, hobbies: string[] };
+// let myself: Person = {
+//     name: "Max",
+//     bankAccount: bankAccount,
+//     hobbies: ["Sports", "Cooking"]
+// };
+// myself.bankAccount.deposit(3000);
+// console.log(myself);
 //# sourceMappingURL=exercise.js.map
