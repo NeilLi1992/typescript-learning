4d6a0cb9d50925733b96018ef4f2f2fefdf2110b
Yong Li <yong.li1992@foxmail.com>
2018-06-17 18:00:43 +0100

    getters & setters

--- app.js
+++ app.js
@@ -40,4 +40,33 @@ var Yong = /** @class */ (function (_super) {
 }(Person));
 var yong = new Yong("yli989");
 console.log(yong);
+// Getters & Setters
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this._species = "Default";
+    }
+    Object.defineProperty(Plant.prototype, "species", {
+        get: function () {
+            return this._species;
+        },
+        // call like a property access
+        set: function (value) {
+            if (value.length > 3) {
+                this._species = value;
+            }
+            else {
+                this._species = "Default";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Plant;
+}());
+var plant = new Plant();
+console.log(plant.species);
+plant.species = "AB";
+console.log(plant.species);
+plant.species = "Green Plant";
+console.log(plant.species);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -33,3 +33,28 @@ class Yong extends Person {
 
 const yong = new Yong("yli989");
 console.log(yong);
+
+// Getters & Setters
+class Plant {
+    private _species: string = "Default";
+
+    get species() {
+        return this._species;
+    }
+
+    // call like a property access
+    set species(value: string) {
+        if (value.length > 3) {
+            this._species = value;
+        } else {
+            this._species = "Default";
+        }
+    }
+}
+
+let plant = new Plant();
+console.log(plant.species);
+plant.species =  "AB";
+console.log(plant.species);
+plant.species = "Green Plant";
+console.log(plant.species);
