c5699d1fb3180652cbc73db5f86054f6355d7277
Yong Li <yong.li1992@foxmail.com>
2018-06-17 15:50:25 +0100

    ES6 features exercise

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-function makeArray(naem) {
-    var args = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        args[_i - 1] = arguments[_i];
-    }
-    return args;
-}
-console.log(makeArray("Yong", 1, 2, 6));
-// Destructing
-var myHobbies = ["Cooking", "Sports"];
-var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
-console.log(hobby1, hobby2);
-var userData = { userName: "Yong", age: 26 };
-var myName = userData.userName, age = userData.age;
-console.log(myName, age);
-// template literals
-var userName = "Max";
-var greeting = "This is a heading!\n    I'm " + userName + "\n";
-console.log(greeting);
+// Exercise 1 - Maybe use an Arrow Function?
+var double = function (value) { return 2 * value; };
+console.log(double(10));
+// Exercise 2 - If only we could provide some default values...
+var greet = function (name) {
+    if (name === void 0) { name = "Max"; }
+    console.log("Hello, " + name);
+};
+greet();
+greet("Anna");
+// Exercise 3 - Isn't there a shorter way to get all these Values?
+var numbers = [-3, 33, 38, 5];
+console.log(Math.min.apply(Math, numbers));
+// Exercise 4 - I have to think about Exercise 3 ...
+var newArray = [55, 20];
+newArray.push.apply(newArray, numbers);
+console.log(newArray);
+// Exercise 5 - That's a well-constructed array.
+var testResults = [3.89, 2.99, 1.38];
+var result1 = testResults[0], result2 = testResults[1], result3 = testResults[2];
+console.log(result1, result2, result3);
+// Exercise 6 - And a well-constructed object!
+var scientist = { firstName: "Will", experience: 12 };
+var firstName = scientist.firstName, experience = scientist.experience;
+console.log(firstName, experience);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,24 +1,29 @@
-function makeArray(naem: string, ...args: number[]) {
-    return args;
-}
-
-console.log(makeArray("Yong", 1, 2, 6));
-
-// Destructing
-const myHobbies = ["Cooking", "Sports"];
-
-const [hobby1, hobby2] = myHobbies;
-
-console.log(hobby1, hobby2);
-
-const userData = {userName: "Yong", age: 26};
-const {userName: myName, age} = userData;
-
-console.log(myName, age);
-
-// template literals
-const userName = "Max";
-const greeting = `This is a heading!
-    I'm ${userName}
-`;
-console.log(greeting);
+// Exercise 1 - Maybe use an Arrow Function?
+var double = value => 2 * value;
+console.log(double(10));
+ 
+// Exercise 2 - If only we could provide some default values...
+var greet = function (name = "Max") {
+    console.log("Hello, " + name);
+};
+greet();
+greet("Anna");
+ 
+// Exercise 3 - Isn't there a shorter way to get all these Values?
+var numbers = [-3, 33, 38, 5];
+console.log(Math.min(...numbers));
+ 
+// Exercise 4 - I have to think about Exercise 3 ...
+var newArray = [55, 20];
+newArray.push(...numbers)
+console.log(newArray);
+ 
+// Exercise 5 - That's a well-constructed array.
+var testResults = [3.89, 2.99, 1.38];
+const [result1, result2, result3] = testResults;
+console.log(result1, result2, result3);
+ 
+// Exercise 6 - And a well-constructed object!
+var scientist = {firstName: "Will", experience: 12};
+const {firstName, experience} = scientist;
+console.log(firstName, experience);
