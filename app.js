dc2262ce2897c1f09190841e679c5ad3acc43b89
Yong Li <yong.li1992@foxmail.com>
2018-06-17 21:52:35 +0100

    private constructors & singletons

--- app.js
+++ app.js
@@ -105,4 +105,19 @@ var newProject = new ITProject();
 console.log(newProject);
 newProject.changeName("Super IT Project");
 console.log(newProject);
+// private constructors
+var OnlyOne = /** @class */ (function () {
+    function OnlyOne(name) {
+        this.name = name;
+    }
+    OnlyOne.getInstance = function () {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne("The Only One");
+        }
+        return OnlyOne.instance;
+    };
+    return OnlyOne;
+}());
+// let wrong = new OnlyOne("The Only One");
+var right = OnlyOne.getInstance();
 //# sourceMappingURL=app.js.map
