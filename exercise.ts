638b07d78ddd1e2d95b9198a8ae7f4c556e816d6
Yong Li <yong.li1992@foxmail.com>
2018-06-17 14:08:49 +0100

    exercise code

---
+++ exercise.ts
@@ -0,0 +1,20 @@
+type AccountType = { money: number, deposit: (val: number) => void };
+
+let bankAccount: AccountType = {
+    money: 2000,
+    deposit(value) {
+        this.money += value;
+    }
+};
+
+type Person = { name: string, bankAccount: AccountType, hobbies: string[] };
+
+let myself: Person = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
