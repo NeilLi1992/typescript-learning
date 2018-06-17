8161ba2e7a9358be5de0af5da412a77c12201088
Yong Li <yong.li1992@foxmail.com>
2018-06-17 15:38:41 +0100

    destructuring array

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ function makeArray(naem) {
     return args;
 }
 console.log(makeArray("Yong", 1, 2, 6));
+// Destructing
+var myHobbies = ["Cooking", "Sports"];
+var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
+console.log(hobby1, hobby2);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -3,3 +3,10 @@ function makeArray(naem: string, ...args: number[]) {
 }
 
 console.log(makeArray("Yong", 1, 2, 6));
+
+// Destructing
+const myHobbies = ["Cooking", "Sports"];
+
+const [hobby1, hobby2] = myHobbies;
+
+console.log(hobby1, hobby2);
