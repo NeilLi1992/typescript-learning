77cc04cac281efa1bbd8be5091607a73069aa226
Yong Li <yong.li1992@foxmail.com>
2018-06-17 11:30:04 +0100

    numbers & booleans

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 "use strict";
 // string
 var myName = 'Yong';
-myName = 28;
+// myName = 28; // error, can't reassign a different type
+// number
+var myAge = 27.5;
+// myAge = 'Max'; // error
+// boolean
+var hasHobbies = false;
+hasHobbies = 1;
--- app.ts
+++ app.ts
@@ -1,3 +1,12 @@
 // string
 let myName = 'Yong';
-myName = 28;
+// myName = 28; // error, can't reassign a different type
+
+// number
+let myAge = 27.5;
+// myAge = 'Max'; // error
+
+// boolean
+let hasHobbies = false;
+// hasHobbies = 1; // still error, TS doesn't do implicit casting
+
